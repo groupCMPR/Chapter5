98340cc0ea0bb4394860145fa265b3df0982f4af
groupCMPR <144290496+groupCMPR@users.noreply.github.com>
2023-09-29 00:18:59 -0700

    Add files via upload

---
+++ ListContainer.h
@@ -0,0 +1,24 @@
+//Names: Neidy Malaga, & Vivian Huynh
+//Date: 9/25/2023
+//Description: Header file for class ListContainer
+
+#pragma once
+#include <string>
+#include <list>
+using namespace std;
+
+class ListContainer
+{
+private:
+	
+
+public:
+	//CONSTRUCTOR
+	ListContainer();
+
+	//ACCESSORS
+
+
+	//MUTATORS
+
+};
