8955affe2fb8e2836936eb53e22e0d68dcd76dfa
groupCMPR <144290496+groupCMPR@users.noreply.github.com>
2023-10-02 10:41:13 -0700

    Update ListContainer.h

--- ListContainer.h
+++ ListContainer.h
@@ -1,32 +1,30 @@
-#pragma once
-//Names: Neidy Malaga, & Vivian Huynh
+//Names: Neidy Malaga & Vivian Huynh
 //Date: 9/25/2023
 //Description: Header file for class ListContainer
 
 #pragma once
-#include "student.h" //for composition
-#include <iostream> //for overloaded << or cout
-#include <string>
-#include <sstream> //for displaying data types
-#include <list> //linked list
+#include "StudentInfo.h" //For composition
+#include <iostream>      //For overloaded << or cout
+#include <string>        //For string
+#include <sstream>       //For displaying data types
+#include <list>          //For linked list
 using namespace std;
 
-class ListContainer{
+class ListContainer {
 private:
-	list <student> Student_List; // Composition, is a list of StudentInfo's objects
-									 //allows you to change elements (by either passing a StudentInfo constructor or obj)
-									 //allows you to display elements: iter->get_ or *iter (which will display overloaded << function of studentInfo)
-
+	list <StudentInfo> Student_List; // Composition, is a list of StudentInfo's objects
+	//                                  allows you to change elements (by either passing a StudentInfo constructor or obj)
+	//                                  allows you to display elements: iter->get_ or *iter (which will display overloaded <<
+	//                                  function of studentInfo)
 
 public:
-	//Default CONSTRUCTOR
-	ListContainer();
-
+	//CONSTRUCTOR
+	ListContainer(); //Default
 
 	//MUTATORS
 	void set_Clear();
 	void set_Resize(const int&);
-	void set_List(const student&, const bool&);
+	void set_List(const StudentInfo&, const bool&);
 	void set_Pop_Front();
 	void set_Pop_Back();
 
@@ -35,8 +33,8 @@ public:
 	string get_Front() const;
 	string get_Back() const;
 	bool get_Empty() const;
-	list<student>::const_iterator  get_First_Iter() const;
+	list<StudentInfo>::const_iterator  get_First_Iter() const;
 
-	//Friend
+	//FRIEND
 	friend ostream& operator<<(ostream&, const ListContainer&);
 };
