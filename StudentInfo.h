f735ac1b92de6d46e1b3765ce8c8385c6014f887
groupCMPR <144290496+groupCMPR@users.noreply.github.com>
2023-10-02 10:42:18 -0700

    Update StudentInfo.h

--- StudentInfo.h
+++ StudentInfo.h
@@ -1,34 +1,36 @@
-//Names: Neidy Malaga, & Vivian Huynh
+//Names: Neidy Malaga & Vivian Huynh
 //Date: 9/25/2023
 //Description: Derived header file for class StudentInfo
 
 #pragma once
-#include <string>
-#include <iostream>
+#include <string>   //For string
+#include <iostream> //For cout
 using namespace std;
 
 class StudentInfo
 {
 private:
-	string name;    //To contain the student name
-	int gradeLevel; //To contain the grade level 0...4
-	double gpa;     //To contain the student's gpa 0.0...4.0
+	string name;       //To contain the student name
+	string gradeLevel; //To contain the grade level
+	double gpa;        //To contain the student's gpa 0.0...4.0
 
 public:
 	//CONSTRUCTOR
-	StudentInfo();
-	StudentInfo(const string&, const int&, const double&);
+	StudentInfo(); //Default
+	StudentInfo(const string&, const string&, const double&); //Argument
 
 	//ACCESSORS
 	string getName() const;
 	string getGradeLevel() const;
-	double getGpa() const;
+	double getGPA() const;
 
 	//MUTATORS
-	void setName(string);
-	void setGradeLevel(int);
-	void setGpa(double);
+	void setName(const string&);
+	void setGradeLevel(const string&);
+	void setGPA(const double&);
 
 	//FRIEND
 	friend ostream& operator<<(ostream&, const StudentInfo&);
+	friend bool operator ==(const StudentInfo&, const StudentInfo&);
+	friend bool operator <(const StudentInfo&, const StudentInfo&);
 };
