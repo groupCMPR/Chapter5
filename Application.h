f3ddbfee377c35074efac078545c996dc63a23fb
groupCMPR <144290496+groupCMPR@users.noreply.github.com>
2023-10-02 10:43:54 -0700

    Update Application.h

--- Application.h
+++ Application.h
@@ -1,9 +1,13 @@
+//Names: Alexis Martinez & Angie Campos
+//Date: 9/28/2023
+//Description: Header file for class Application
+
 #pragma once
-#include <list> //for list
-#include <map> //for maps, used in frequencies
-#include <iostream> //for overloaded function
-#include <sstream> //for displaying data types, used in frequencies
-#include <string> //for string
+#include <list>     //For list
+#include <map>      //For maps, used in frequencies
+#include <iostream> //For overloaded function
+#include <sstream>  //For displaying data types, used in frequencies
+#include <string>   //For string
 using namespace std;
 
 class Application {
@@ -12,16 +16,17 @@ private:
 	list<value_type> user_val_list;
 
 public:
-	//Default constructor
-	Application(); 
+	//CONSTRUCTOR
+	Application(); //Default
 
-	//MUTATOR:
+	//MUTATORS
 	void set_List(const value_type&);
 	void delete_List(const value_type&);
 
-	//ACCESSOR:
+	//ACCESSORS
 	bool get_Empty() const;
 	string get_Freq() const;
 
-	friend ostream& operator <<(ostream& out, const Application &);
+	//FRIEND
+	friend ostream& operator <<(ostream& out, const Application&);
 };
