8c1a3ac2a1419337e4be302db6f4d49a59caa7b1
groupCMPR <144290496+groupCMPR@users.noreply.github.com>
2023-10-02 10:43:31 -0700

    Update StudentInfo.cpp

--- StudentInfo.cpp
+++ StudentInfo.cpp
@@ -1,90 +1,82 @@
-//Names: Neidy Malaga, & Vivian Huynh
+//Names: Neidy Malaga & Vivian Huynh
 //Date: 9/25/2023
 //Description: Derived cpp file for class StudentInfo
 
 #include "StudentInfo.h"
 
-const string classLevel[] = { "Unknown", "Freshman", "Sophmore", "Junior", "Senior" };
-
+//---------------------------------------------------------------------------
+//                               CONSTRUCTORS
+//---------------------------------------------------------------------------
 //Precondition : N/A
 //Postcondition: Initalize the private members
-StudentInfo::StudentInfo() : name("Unknown"), gradeLevel(0), gpa(0.0)
+StudentInfo::StudentInfo() : name("Unknown"), gradeLevel("Unknown"), gpa(0.0)
+{}
+//Precondition : N/A
+//Postcondition: Change the private members
+StudentInfo::StudentInfo(const string& new_Name, const string& level, const double& new_Gpa) : name(new_Name), gradeLevel(level), gpa(new_Gpa)
 {}
 
-StudentInfo::StudentInfo(const string& new_Name, const int& level, const double& new_Gpa) : name(new_Name), gradeLevel(level), gpa(new_Gpa)
-{}
-
-//============================================================
-// Accessors Section
-//============================================================
+//---------------------------------------------------------------------------
+//                                ACCESSORS
+//---------------------------------------------------------------------------
 //Precondition : N/A
 //Postcondition: Return the private member name
-string StudentInfo::getName() const
-{
+string StudentInfo::getName() const{
 	return name;
 }
 //Precondition : N/A
 //Postcondition: Return the private member gradeLevel to go into the classLevel
-string StudentInfo::getGradeLevel() const
-{
-	return classLevel[gradeLevel];
+string StudentInfo::getGradeLevel() const{
+	return gradeLevel;
 }
 //Precondition : N/A
 //Postcondition: Return the private member gpa 
-double StudentInfo::getGpa() const
-{
+double StudentInfo::getGPA() const{
 	return gpa;
 }
 
-//============================================================
-// Mutators Section
-//============================================================
+//---------------------------------------------------------------------------
+//                                MUTATORS
+//---------------------------------------------------------------------------
 //Precondition : Passing in newName in valid string inputs
 //Postcondition: Changing private member name
-void StudentInfo::setName(string newName)
-{
+void StudentInfo::setName(const string& newName){
 	name = newName;
 }
 //Precondition : Passing in newGradeLevel between 0...4
 //Postcondition: Changing private member gradeLevel
-void StudentInfo::setGradeLevel(int newGradeLevel)
-{
+void StudentInfo::setGradeLevel(const string& newGradeLevel){
 	gradeLevel = newGradeLevel;
 }
 //Precondition : Passing in newGpa between 0.0...4.0
 //Postcondition: Changing private member gpa
-void StudentInfo::setGpa(double newGpa)
-{
+void StudentInfo::setGPA(const double& newGpa){
 	gpa = newGpa;
 }
 
-//============================================================
-// Friend Section
-//============================================================
+//---------------------------------------------------------------------------
+//                                FRIENDS
+//---------------------------------------------------------------------------
+//overloading operator <<
 //Precondition : Passing in out and valid obj of the class
 //Postcondition: Outputting the display of student info (Name, Grade Level, GPA)
-ostream& operator<<(ostream& out, const StudentInfo& obj)
-{
-	out << obj.getName() << ", " << obj.getGradeLevel() << ", " << obj.getGpa();
-	return out;
+ostream& operator<<(ostream& outs, const StudentInfo& obj){
+	outs << obj.getName() << ", " << obj.getGradeLevel() << ", " << obj.getGPA();
+	return outs;
 }
-
 //overloading operator == 
-//Precondition : two StudentInfo classes 
-//Postcondition: returns true if all private members are same, else false
-friend bool operator ==(const StudentInfo& obj1, const StudentInfo& obj2)
-{
-	if ((obj1.GPA == obj2.GPA) && (obj1.gradeLevel == obj2.gradeLevel) && (obj1.name == obj2.name))
+//Precondition : Two StudentInfo classes 
+//Postcondition: Returns true if all private members are same, else false
+bool operator ==(const StudentInfo& obj1, const StudentInfo& obj2){
+	if ((obj1.gpa == obj2.gpa) && (obj1.gradeLevel == obj2.gradeLevel) && (obj1.name == obj2.name))
 		return true;
 	else
 		return false;
 }
-
 //overloading operator <
-//Precondition : two StudentInfo classes 
-
-friend bool operator <(const  StudentInfo& obj1, const  StudentInfo& obj2)
-{
+//Precondition : Two StudentInfo classes 
+//Postcondition: Returns true if name in second object is first in order, else false
+bool operator <(const StudentInfo& obj1, const StudentInfo& obj2){
 	if (obj1.name < obj2.name)
 		return true;
 	else
